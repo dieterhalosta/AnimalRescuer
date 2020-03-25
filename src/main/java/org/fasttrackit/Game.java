89883d77c227d9007f0e451fde827e4bcb9fae12
Dieter Halosta <dieter.halosta@yahoo.com>
2020-03-25 17:11:00 +0200

    Started to work on the Game class

--- src/main/java/org/fasttrackit/Game.java
+++ src/main/java/org/fasttrackit/Game.java
@@ -1,32 +1,38 @@
 package org.fasttrackit;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Game {
-    Rescuer rescuer;
-    Animal animal;
-    Vet vet;
+//    Rescuer rescuer;
+//    Animal animal;
+//    Vet vet;
 
     private Animal[] animals = new Animal[2];
+    private List<Food> availableFood = new ArrayList<Food>();
+    private PlayTime[] availableActivities = new PlayTime[4];
 
     public void start(){
         System.out.println("Let's start!");
-        //initializeUser();
+        String playerName = initializeUser();
+        Human player = new Rescuer(playerName);
+        System.out.println("Hello " + player.getName());
         initializeAnimals();
         displayAnimals();
         Animal selectedAnimal = getSelectedAnimalFromUser();
         System.out.println("Selected animal: " + selectedAnimal.getName());
     }
 
-//    private String initializeUser() {
-//        System.out.println("Please enter your name.");
-//        Scanner scanner = new Scanner(System.in);
-//        String player = rescuer.setName(scanner.nextLine());
-//        return scanner.nextLine();
-//    }
+    private String initializeUser() {
+        System.out.println("Please enter your name.");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+
+    }
 
     private void initializeAnimals() {
-        Animal dog = new Animal("Appel");
+        Animal dog = new Animal("Apple");
         dog.setAge(3);
         dog.setHealthLevel(3);
         dog.setHungerLevel(10);
